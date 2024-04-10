34d878adc492249557fc212ac3875bc0e7a568c9
Dishant Yadav <dishantrao11@gmail.com>
2024-04-11 03:20:01 +0530

    done with most of react components

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Button({
+    children , 
+    type = 'button',
+    bgcolor = 'bg-blue-600',
+    textColor = 'text-white',
+    className = '',
+    // props will signify more property that user will pass
+    ...props
+    }) {
+  return (
+    <Button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
+        {children}
+    </Button>
+  )
+}
+
+export default Button
